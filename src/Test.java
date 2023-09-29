cb777efc141d93a26ca6bd76d16723e659059940
Yurii <y.o.fedorov@student.khai.edu>
2023-09-29 10:26:40 +0300

    Resolved conflict 3/3

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args){
         System.out.println("Javaeducation");
+        //comment on second string of the func
+        System.out.println("DICT");
     }
 }
