6b5c00a3629a9f69c63e83f1ba606f814464903f
Yurii <y.o.fedorov@student.khai.edu>
2023-10-13 10:04:03 +0300

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,14 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args){
         System.out.println("Hello! my name is T");
         System.out.println("I was created in 2023");
+        System.out.println("Please, remind me your name");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Name:");
+        String name = input.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
