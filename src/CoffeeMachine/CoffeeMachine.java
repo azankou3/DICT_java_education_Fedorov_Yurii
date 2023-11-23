0bdabbdf4fb02008826aa82a2a373f6bfca96e7e
Yurii <y.o.fedorov@student.khai.edu>
2023-11-23 23:58:03 +0200

    CoffeeMachine 2-nd Stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,13 +1,28 @@
 package CoffeeMachine;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args){
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(">");
+        int  cupsInput = scanner.nextInt();
+
+        HashMap<String, Integer> ForOneCup = new HashMap<>();
+        ForOneCup.put("Water", 200);
+        ForOneCup.put("Milk", 50);
+        ForOneCup.put("CoffeeBeans", 15);
+
+        int NeedWater = ForOneCup.get("Water") * cupsInput;
+        int NeedMilk = ForOneCup.get("Milk") * cupsInput;
+        int NeedCoffeeBeans = ForOneCup.get("CoffeeBeans") * cupsInput;
+
+        System.out.println("For " + cupsInput + " of coffee you will need:");
+        System.out.println(NeedWater + " ml of water");
+        System.out.println(NeedMilk + " ml of milk");
+        System.out.println(NeedCoffeeBeans + " g of coffee beans");
+
     }
 }
